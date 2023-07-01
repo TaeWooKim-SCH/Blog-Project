0030373a43f15e3084bdf6536f789f39f9b25388
TaeWooKim-SCH <79956107+TaeWooKim-SCH@users.noreply.github.com>
2023-07-01 16:45:07 +0900

    Add files via upload

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -3,14 +3,13 @@
 import dynamic from 'next/dynamic';
 import style from '../_styles/Post.module.css';
 
-const MdEditor = dynamic(() => import('../_components/MdEditor'), { ssr: false });
-const MdEdi = dynamic(() => import('../_components/MdEdi'), { ssr: false });
+const MdEdit = dynamic(() => import('../_components/MdEdit'), { ssr: false });
 
 export default function Post() {
 
   return (
     <section className={style.postSection}>
-      <MdEdi />
+      <MdEdit />
     </section>
   );
 }
