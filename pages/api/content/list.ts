1cee3d39c25820af4e4a6b3b52768cc935f5218d
TaeWooKim-SCH <zop1234@hanmail.net>
2023-08-08 17:05:14 +0900

    [fix] 서버 사이드 렌더링 수정

--- pages/api/content/list.ts
+++ pages/api/content/list.ts
@@ -8,9 +8,9 @@ export default async function handler(
   ): Promise<void> {
   const db: Db = await connectDB();
   if (req.query.category !== 'All') {
-    const result: object | null = await db.collection('contents').findOne(req.query);
-    if (!result) return res.status(200).json(result);
-    return res.status(200).json([result]);
+    const result: object | null = await db.collection('contents').find(req.query).toArray();
+    if (!result) return res.status(200).json([]);
+    return res.status(200).json(result);
   }
   const result: object = await db.collection('contents').find().toArray();
   return res.status(200).json(result);
